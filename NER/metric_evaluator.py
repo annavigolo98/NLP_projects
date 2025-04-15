4c30f85aff1fa7a565a8a5c7d50f43bee8be0e25
Anna Vigolo <vigoloanna1998@gmail.com>
2025-04-15 17:34:53 +0200

    metric NER updated

--- NER/metric_evaluator.py
+++ NER/metric_evaluator.py
@@ -12,12 +12,18 @@ class MetricEvaluator(BaseModel):
             label_names = ['O', 'B-PER', 'I-PER', 'B-ORG', 'I-ORG', 'B-LOC', 'I-LOC', 'B-MISC', 'I-MISC']
  
             logits, labels = logits_and_labels
-            preds = np.argmax(logits, axis=-1)
+            preds=np.argmax(logits, axis=-1)
 
-            labels_str = [[label_names[t] for t in label if t  !=-100] for label in labels]
-            preds_str = [[label_names[p] for p,t in zip(pred, label) if t !=-100 ] for pred, label in zip(preds, labels)]
-
-            metric_evaluation = metric.compute(predictions = preds_str, references = labels_str)
+            #remove -100 from labels and predictions
+            #and convert labels_ids to label names
+            str_labels=[
+                [label_names[t] for t in label if t!=-100] for label in labels
+            ]
+            # Remove -100 only if the true label is -100
+            str_preds = [
+                [label_names[p] for p, t in zip(pred, targ) if t!=-100] for pred, targ in zip(preds, labels)
+            ]
+            metric_evaluation = metric.compute(predictions=str_preds, references=str_labels)
             return {'precision': metric_evaluation['overall_precision'],
                     'recall': metric_evaluation['overall_recall'],
                     'f1': metric_evaluation['overall_f1'],
