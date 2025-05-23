37cf10abf9763bc2575852999b2ecbcfcc5868c2
annavigolo98 <vigoloanna1998@gmail.com>
2025-05-23 11:48:59 +0200

    plots on translator added

--- 52_transformers_test.py
+++ 52_transformers_test.py
@@ -3,7 +3,7 @@ from transformer.seq2seq_service import Seq2SeqService
 def main():
 
     service = Seq2SeqService()
-    sentence_to_translate = 'My name is Roberto.'
+    sentence_to_translate = 'Can you speak Spanish?'
     service.translate_sentence(sentence_to_translate)
 
 
